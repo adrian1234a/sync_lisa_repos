580a16c0c20baca20434aeaa5c777057f960e1c1
adrian1234a <adri16072002@gmail.com>
2023-08-11 20:15:56 +0200

    Update clone.sh
    Change to official repos

--- clone.sh
+++ clone.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git clone https://gitlab.com/PixelOS-Devices/vendor_xiaomi_lisa-firmware.git -b thirteen vendor/xiaomi/lisa-firmware
 git clone https://gitlab.com/PixelOS-Devices/vendor_xiaomi_lisa.git -b thirteen vendor/xiaomi/lisa
-gh repo clone Amit152505/vendor_xiaomi_camera vendor/xiaomi/camera
+git clone https://github.com/adrian1234a/vendor_xiaomi_camera vendor/xiaomi/camera
 git clone https://github.com/crdroidandroid/android_hardware_xiaomi.git -b 13.0-MI8350 hardware/xiaomi
-gh repo clone adrian1234a/kernel_xiaomi_lisa kernel/xiaomi/lisa
-gh repo clone adrian1234a/device_xiaomi_lisa device/xiaomi/lisa
+git clone https://github.com/crdroidandroid/kernel_xiaomi_lisa kernel/xiaomi/lisa
+git clone https://github.com/crdroidandroid/android_device_xiaomi_lisa device/xiaomi/lisa
